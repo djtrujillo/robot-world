f959bd3b79afef72a6f880611c3c813ad40159dc
djtrujillo <danny.j.trujillo@gmail.com>
2017-08-16 09:00:04 -0600

    Final functionality

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,45 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :dashboard
+  end
+
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  post '/robots' do
+    robot = Robot.new(params)
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+  set :method_override, true
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  delete '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect '/robots'
+  end
+
+end
